78a3fb4adafbf684f1ff751a921258539b93d15a
AR <aarokan@gmail.com>
2024-10-01 13:23:22 +0300

    solve Lab_variables

---
+++ main.py
@@ -0,0 +1,11 @@
+myTeam = "Alnasser"
+
+# define int and float and use type hint
+x: int = 4
+y: float = 7.85
+
+# Assign One Value to Multiple Variables
+carColor = bandColor = shirtColor = "red"
+
+# Assign multiple Values to Multiple Variables
+apple, kiwi, banana = "red", "green", "yellow"
