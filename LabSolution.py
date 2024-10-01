232aeba553eba7681661d29659efdc723be9a3b7
Reema <reemaalbrahim28@gmail.com>
2024-10-01 13:47:32 +0300

    Football team updates

---
+++ LabSolution.py
@@ -0,0 +1,19 @@
+#Define a variable that has a value of your favourite football team.
+favorite_team="Real Madrid"
+
+#Define a variable “x“  has an integer value and a variable “y” has a float value.
+x:int=23
+y:float=29.5
+
+#Assign One Value to Multiple Variables (choose random names & values).
+a = b = c = "My favorite football team"
+
+#Assign multiple Values to Multiple Variables (choose random names & values).
+name, age, height = "Reema", 23, 169
+
+#Printing the results 
+print("Favorite Football Team:", favorite_team)
+print("Integer value (x):", x)
+print("Float value (y):", y)
+print("One Value to Multiple Variables:", a, b, c)
+print("Multiple Values to Multiple Variables:", name, age, height)
