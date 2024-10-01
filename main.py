e2fc8ecf138b6054d75e99be992034c3b4acf643
Atiq <abdullah.s.alatiq@gmail.com>
2024-10-01 13:32:37 +0300

    Solve the LAB_VARIABLES

---
+++ main.py
@@ -0,0 +1,12 @@
+# Define a variable that has a value of your favourite football team
+team = "Hilal"
+
+# Define a variable “x“ that has an integer value and a variable “y” that has a float value
+x:int = 7
+y:float = 3.14
+
+# Assign One Value to Multiple Variables (choose random names & values)
+a = b = c = 21
+
+# Assign multiple Values to Multiple Variables (choose random names & values)
+name , course , time = "Abdullah" , "python" , 10
