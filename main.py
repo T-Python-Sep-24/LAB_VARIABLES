56b433cf6b0be9ea443ac950497a10cec85dd8f2
Areej Shareefi <aomsh1420@gmail.com>
2024-10-01 13:45:10 +0300

    solving the LAB_VARIABLES

---
+++ main.py
@@ -0,0 +1,12 @@
+# My favourite football team
+football_team="Etihad Team"
+
+# an int x variable & float y variable
+x:int =5
+y:float=0.5
+
+# Assigning One Value to Multiple Variables 
+a = b = c = 10
+
+#Assigning multiple Values to Multiple Variables
+d , e, f, = 20, 30, 40
