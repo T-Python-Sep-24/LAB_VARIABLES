f1c01f2742f72896083641a6296de68acd3eadc5
alhanouf alluhaydan <alhanoufkhalaf1@gmail.com>
2024-10-01 13:40:45 +0300

    solved

---
+++ main.py
@@ -0,0 +1,27 @@
+
+#Define a variable that has a value of your favourite football team.
+
+footballTeam = "Real Madrid "
+print(footballTeam)
+
+#Define a variable “x“  has an integer value and a variable “y” has a float value.
+
+X:int = 7
+Y:float = 7.7
+
+print(type(X))
+print(type(Y))
+
+#Assign One Value to Multiple Variables (choose random names & values).
+
+Team1 = Team2 = Team3 = "lose"
+
+print(Team1,Team2,Team3)
+
+#Assign multiple Values to Multiple Variables (choose random names & values).
+
+player1 , player2 , player3 = 12 , 34 ,5
+
+print(player1,player2,player3)
+
+
