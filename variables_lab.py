648bb4c83477eeb34f93c94a6611e8d5d680c704
HmzhDubh <meezo.2015@hotmail.com>
2024-10-01 13:21:16 +0300

    completed variables lab

---
+++ variables_lab.py
@@ -0,0 +1,14 @@
+# Hamzah Dabbah
+
+myDreadTeam = "Real Madrid"
+print(myDreadTeam)
+
+x = 77
+y = 4.68
+print("x = ", x, "and y = ", y)
+
+name = age = email = "not determined yet"
+print(name, email, age)
+
+name, age, email = "Hmzh", 23, "hamza.helal.d@gmail.com"
+print("my name is", name, ", and I am",age, ", contact me on", email)
