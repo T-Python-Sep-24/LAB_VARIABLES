17c6e1ae90c3b637444f47b181656dcb0ae7b1d9
Khalid ALMaghyadi <K95.job@gmail.com>
2024-10-01 13:35:59 +0300

    All done

---
+++ main.py
@@ -0,0 +1,12 @@
+myFavouriteFootballTeam = "Alhilal"
+x :int = 15
+y = 1.5
+Variable1 =  Variable2 = 11
+VariableSingle1 , VariableSingle2 = 10 , 5.0
+
+print("This is my Favourite team:", myFavouriteFootballTeam)
+print("THis is test for variable x:",x)
+print("THis is test for variable y:",y)
+print("This is test for  Value to Multiple Variables:", Variable1 , Variable2)
+print("This is test for multiple Values to Multiple Variables:", VariableSingle1 , VariableSingle2 )
+print("This is test for multiple Values to Multiple Variables:", VariableSingle1 , VariableSingle2 )
