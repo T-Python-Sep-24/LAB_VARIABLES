89e8fc7d87ee0ae3089d107b49f187d7a4ed7c42
Alhanouf <hnno3040@gmail.com>
2024-10-01 13:38:06 +0300

    Start working in the main file

---
+++ main.py
@@ -0,0 +1,9 @@
+favourite_football_team = "Etihad"
+print(favourite_football_team)
+x:int = 23
+y:float = 25.3
+print(x, y)
+var1 = var2 = var3 = 26
+print(var1, var2, var3)
+var4, var5, var6,= 22, 24, 27
+print(var4, var5, var6)
