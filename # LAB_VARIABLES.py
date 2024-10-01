f651130e6369eed80e2472a2f5cbdb083d9499a9
Bayan Alharbi <bayanlharbi@gmail.com>
2024-10-01 14:30:25 +0300

    README file

---
+++ # LAB_VARIABLES.py
@@ -0,0 +1,16 @@
+# lab_variables.py
+
+# LAB_VARIABLES
+
+favorite_football_team = "Barcelona"
+
+x = 25
+y = 4.5
+
+name1 = name2 = name3 = "John Doe"  # Assigning the same string to multiple variables
+
+player_name, player_number, player_height = "Lionel Messi", 10, 1.70
+print(f"Favorite Football Team: {favorite_football_team}")
+print(f"x (Integer): {x}, y (Float): {y}")
+print(f"name1: {name1}, name2: {name2}, name3: {name3}")
+print(f"Player Name: {player_name}, Player Number: {player_number}, Player Height: {player_height}")
