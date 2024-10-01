8af46da2010f64e35778f5c20ebfebe5e6135f08
Hessa <Hessaalmasaad@gmail.com>
2024-10-01 19:47:03 +0300

    python Lab1

---
+++ main.py
@@ -0,0 +1,27 @@
+#Define a variable that has a value of your favourite football team.
+favorite_football_team="Alhilal Team"
+
+#Define a variable “x“ has an integer value and a variable “y” has a float value.
+x=10
+y=50.5
+
+#Assign One Value to Multiple Variables (choose random names & values).
+a = b = c = d = "6 feet under"
+
+#Assign multiple Values to Multiple Variables (choose random names & values).
+z, h, v, p = "OOP", 70.9, 80, "Python"
+
+# Print the results
+print(favorite_football_team)
+print("x =", x, "\ny =", y)
+print("Assigning One Value to Multiple Variables")
+print("Value assigned to a is", a)
+print("Value assigned to b is", b)
+print("Value assigned to c is", c)
+print("Value assigned to d is", d)
+print("-----------------------------------")
+print("Assigning Multiple Values to Multiple Variables")
+print("Value assigned to z is", z)
+print("Value assigned to h is", h)
+print("Value assigned to v is", v)
+print("Value assigned to p is", p)
