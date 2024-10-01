a147679e411282f4a7a6029ec80c358c0f924456
Ghaliah <ghalia.banouna@gmail.com>
2024-10-01 13:20:50 +0300

    Lab Work Solution

---
+++ LabWork.py
@@ -0,0 +1,12 @@
+#Defining a varibale that had my fav football team name
+footballTeam = "Barcelona"
+
+#Defining two variables; one contains an integer number and the other contains a float number
+x = 7
+y = 7.7
+
+#Assigning one value to mutiple variables
+name1 = name2 = name3 = "Ghaliah"
+
+#Assigning multiple values to multiple variables
+vari1, vari2, vari3 = "Hello", 77, True
