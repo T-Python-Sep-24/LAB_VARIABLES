856ad4e61ec45df9d4b2eb3d144a1b54b29ca628
SalehBinHamed <Saleh.Alrashidi.H@gmail.com>
2024-10-01 13:39:56 +0300

    done

---
+++ Saleh.py
@@ -0,0 +1,12 @@
+from unittest.util import three_way_cmp
+
+favFT="FCbarcalona"
+x,y= int(5) , float(7.5)
+print(f"my fav football time is {favFT} won champinse "
+      f"laige {x} and my fav player is number {y}")
+print(type(x))
+print(type(y))
+one="lsdl"
+two=one
+three=one
+print(one , two , three)
