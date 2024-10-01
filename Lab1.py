60e39f9fcb6458abfc00e3648632fad2ff2b8172
KhuloodGhazi <khulood.gmo@gmail.com>
2024-10-01 13:34:25 +0300

    This is my answer for Lab 1

---
+++ Lab1.py
@@ -0,0 +1,23 @@
+#Step number 1
+
+myFootball = "Al-Hillal"
+
+#Step number 2
+
+x:int = 20
+y:float = 12.50
+
+#Step number 3
+
+x1 = y1 = z1 = 100
+
+#Step number 4
+
+v1, v2, v3, = "Value 1", "Value 2", "Value 3"
+
+print(myFootball)
+print(x)
+print(y)
+print(x1, y1, z1)
+print(v1, v2, v3)
+
